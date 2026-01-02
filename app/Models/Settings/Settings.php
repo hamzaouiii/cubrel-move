d40e5c0fef9674bfcb0ae9b80dee8ce81858ab89
simo hamzaoui <simo.hamzaoui@gmail.com>
2026-01-02 15:21:34 +0100

    show only active setting items

--- app/Models/Settings/Settings.php
+++ app/Models/Settings/Settings.php
@@ -9,40 +9,40 @@ use App\Concerns\HasTranslatableLabel;
 
 class Settings extends Model
 {
-    use HasFactory, HasTranslatableLabel;
+  use HasFactory, HasTranslatableLabel;
 
-    /**
-     * Use UUIDs instead of auto-incrementing integers.
-     */
-    public $incrementing = false;
+  /**
+   * Use UUIDs instead of auto-incrementing integers.
+   */
+  public $incrementing = false;
 
-    /**
-     * The "type" of the primary key ID.
-     *
-     * @var string
-     */
-    protected $keyType = 'string';
+  /**
+   * The "type" of the primary key ID.
+   *
+   * @var string
+   */
+  protected $keyType = 'string';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        // TODO: add your module fields here
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    // TODO: add your module fields here
+  ];
 
-    protected static function booted(): void
-    {
-        static::creating(function (self $model) {
-            if (! $model->getKey()) {
-                $model->setAttribute($model->getKeyName(), (string) Str::uuid());
-            }
-        });
-    }
-        public function items()
-    {
-        return $this->hasMany(SettingItem::class, 'setting_id');
-    }
+  protected static function booted(): void
+  {
+    static::creating(function (self $model) {
+      if (! $model->getKey()) {
+        $model->setAttribute($model->getKeyName(), (string) Str::uuid());
+      }
+    });
+  }
+  public function items()
+  {
+    return $this->hasMany(SettingItem::class, 'setting_id')
+      ->where('active', true);
+  }
 }
-
