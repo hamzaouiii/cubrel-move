b03f5dc1d1572bd55879465b993417bb6c95a1e3
simo hamzaoui <simo.hamzaoui@gmail.com>
2026-03-02 09:35:50 +0100

    Updated Alerts design

--- resources/js/Pages/Components/Globals/Alerts.vue
+++ resources/js/Pages/Components/Globals/Alerts.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, onUnmounted, ref, computed } from "vue";
+import { onUnmounted, ref, computed, watch } from "vue";
 
 const timers = ref({});
 
@@ -18,7 +18,7 @@ const normalizedAlerts = computed(() =>
     dismissible: a.dismissible ?? false,
     progressable: a.progressable ?? false,
     duration: a.duration ?? 5000,
-  }))
+  })),
 );
 
 const closeAlert = (id) => {
@@ -32,6 +32,24 @@ const closeAlert = (id) => {
     delete timers.value[id];
   }
 };
+
+watch(
+  () => normalizedAlerts.value,
+  (alerts) => {
+    alerts.forEach((alert) => {
+      if (alert.progressable && alert.duration && !timers.value[alert.id]) {
+        timers.value[alert.id] = setTimeout(() => {
+          closeAlert(alert.id);
+        }, alert.duration);
+      }
+    });
+  },
+  { immediate: true, deep: true },
+);
+
+onUnmounted(() => {
+  Object.values(timers.value).forEach(clearTimeout);
+});
 </script>
 <template>
   <div class="alerts">
@@ -42,15 +60,20 @@ const closeAlert = (id) => {
       :class="`alerts__item--${alert.type}`"
     >
       <div class="alerts__item__message">
-        <span>
-          {{ alert.message }}
-        </span>
+        {{ alert.message }}
       </div>
+
       <div class="alerts__item__close">
         <span v-if="alert.dismissible !== false" @click="closeAlert(alert.id)">
           <i class="fa-solid fa-times"></i>
         </span>
       </div>
+
+      <div
+        v-if="alert.progressable"
+        class="alerts__item__progress"
+        :style="{ animationDuration: alert.duration + 'ms' }"
+      />
     </div>
   </div>
 </template>
