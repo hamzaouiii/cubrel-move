50fa61cae195d5b4e8755e367bbfd951be7973c2
simo hamzaoui <simo.hamzaoui@gmail.com>
2026-06-18 22:44:55 +0200

    moved logion image to resources

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  server: {
+    port: 4173,
+  },
   plugins: [
     laravel({
       input: "resources/js/app.js",
