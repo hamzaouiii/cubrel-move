e29885dcd0ca65b82cdcb06c4bc5b1fcdba0827c
simo hamzaoui <simo.hamzaoui@gmail.com>
2026-07-14 11:02:24 +0200

    login rate_limit

--- lang/de/globals.php
+++ lang/de/globals.php
@@ -63,6 +63,7 @@ return [
         'invalid_credentials' => 'Benutzername oder Passwort falsch.',
         'invalid_email' => 'E-Mail-Adresse ist falsch.',
         'user_inactive' => 'Benutzer ist nicht aktiv. Bitte kontaktieren Sie Ihren Administrator',
+        'throttled' => 'Zu viele Anmeldeversuche. Bitte versuchen Sie es in :seconds Sekunden erneut.',
         'setup_title' => 'Instanz einrichten',
         'setup_subtitle' => 'Erstellen Sie das Root-Konto, um die Einrichtung von Cubrel abzuschließen.',
         'setup_invalid' => 'Dieser Einrichtungslink ist ungültig, abgelaufen oder wurde bereits verwendet.',
