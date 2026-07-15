596a0583f6579d7da09698049f76fa89b5b8b25d
simo hamzaoui <simo.hamzaoui@gmail.com>
2026-07-15 12:33:14 +0200

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
-
+git config core.fileMode false
 git pull origin main
 composer install --no-dev --optimize-autoloader
 php artisan migrate --force
 php artisan optimize
 npm ci && npm run build
+chmod -R 755 . 
+chown -R www-data:www-data .
 php artisan queue:restart
-
+php artisan storage:link
 echo "Deploy done."
+~                          
