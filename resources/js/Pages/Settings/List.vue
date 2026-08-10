81841e645b4c61953a70548c1650b47f73a1f14c
Simo Hamzaoui <simo.hamzaoui.1993@gmail.com>
2026-08-10 14:03:36 +0200

    split settings to two cols

--- resources/js/Pages/Settings/List.vue
+++ resources/js/Pages/Settings/List.vue
@@ -5,11 +5,11 @@ import SettingsLayout from "@/Layouts/SettingsLayout.vue";
 import { ref, computed, getCurrentInstance } from "vue";
 
 defineOptions({
-  layout: [AppLayout, SettingsLayout],
+    layout: [AppLayout, SettingsLayout],
 });
 
 const pageProps = defineProps({
-  settings: Object,
+    settings: Object,
 });
 const appSettings = usePage().props.appSettings;
 const { proxy } = getCurrentInstance();
@@ -17,105 +17,125 @@ const t = proxy.$t;
 
 const search = ref("");
 const filteredSettings = computed(() => {
-  if (!search.value) {
-    return pageProps.settings;
-  }
-  const term = search.value.toLowerCase();
-  const filteredGroups = {};
-  Object.entries(pageProps.settings).forEach(([groupKey, group]) => {
-    if (!group || !group.items) return;
-    const groupMatches =
-      (t(group.label) && t(group.label).toLowerCase().includes(term)) ||
-      (group.description && group.description.toLowerCase().includes(term)) ||
-      groupKey.toLowerCase().includes(term);
+    if (!search.value) {
+        return pageProps.settings;
+    }
+    const term = search.value.toLowerCase();
+    const filteredGroups = {};
+    Object.entries(pageProps.settings).forEach(([groupKey, group]) => {
+        if (!group || !group.items) return;
+        const groupMatches =
+            (t(group.label) && t(group.label).toLowerCase().includes(term)) ||
+            (group.description &&
+                group.description.toLowerCase().includes(term)) ||
+            groupKey.toLowerCase().includes(term);
 
-    const filteredItems = {};
+        const filteredItems = {};
 
-    Object.entries(group.items).forEach(([itemKey, item]) => {
-      if (!item) return;
+        Object.entries(group.items).forEach(([itemKey, item]) => {
+            if (!item) return;
 
-      const itemMatches =
-        (t(item.label) && t(item.label).toLowerCase().includes(term)) ||
-        (item.description && item.description.toLowerCase().includes(term)) ||
-        (item.name && item.name.toLowerCase().includes(term)) ||
-        (item.path && item.path.toLowerCase().includes(term)) ||
-        itemKey.toLowerCase().includes(term);
+            const itemMatches =
+                (t(item.label) && t(item.label).toLowerCase().includes(term)) ||
+                (item.description &&
+                    item.description.toLowerCase().includes(term)) ||
+                (item.name && item.name.toLowerCase().includes(term)) ||
+                (item.path && item.path.toLowerCase().includes(term)) ||
+                itemKey.toLowerCase().includes(term);
 
-      if (itemMatches) {
-        filteredItems[itemKey] = item;
-      }
+            if (itemMatches) {
+                filteredItems[itemKey] = item;
+            }
+        });
+
+        if (groupMatches || Object.keys(filteredItems).length > 0) {
+            filteredGroups[groupKey] = {
+                ...group,
+                items: groupMatches ? group.items : filteredItems,
+            };
+        }
     });
 
-    if (groupMatches || Object.keys(filteredItems).length > 0) {
-      filteredGroups[groupKey] = {
-        ...group,
-        items: groupMatches ? group.items : filteredItems,
-      };
-    }
-  });
-
-  return filteredGroups;
+    return filteredGroups;
 });
 
 const clearSearch = () => {
-  search.value = "";
+    search.value = "";
 };
+
+const settingsColumns = computed(() => {
+    const columns = [[], []];
+    Object.entries(filteredSettings.value).forEach(([key, group], index) => {
+        columns[index % 2].push([key, group]);
+    });
+    return columns;
+});
 </script>
 
 <template>
-  <Head>
-    <title>{{ $t("settings.label") }} - Cubrel</title>
-  </Head>
-  <div
-    class="settings"
-    :style="{
-      '--primary-color': appSettings.primary_color,
-      '--secondary-color': appSettings.secondary_color,
-    }"
-  >
-    <div class="settings__header">
-      <div class="settings__header__title">
-        <h3 class="settings__header__title__text">
-          {{ $t("settings.label") }}
-        </h3>
-      </div>
-      <div class="settings__header__search">
-        <input
-          v-model="search"
-          class="settings__header__search__field"
-          :placeholder="t('settings.search_placeholder')"
-        />
-        <i
-          v-if="search"
-          class="fa-solid fa-xmark settings__header__search__clear"
-          @click="clearSearch"
-        ></i>
-      </div>
-    </div>
-    <div class="settings__list">
-      <div v-for="s in filteredSettings" class="settings__list__section">
-        <div class="settings__list__section__header">
-          <div class="settings__list__section__header__title">
-            <h6>{{ $t(s.label) }}</h6>
-          </div>
-          <div class="settings__list__section__header__desc">
-            <p>{{ $t(s.description) }}</p>
-          </div>
-        </div>
-        <div class="settings__list__section__links">
-          <Link v-for="i in s.items" :href="i.path">
-            <i :class="i.icon"></i>
-            <span class="label">{{ $t(i.label) }}</span>
-          </Link>
-        </div>
-      </div>
-    </div>
+    <Head>
+        <title>{{ $t("settings.label") }} - Cubrel</title>
+    </Head>
     <div
-      v-if="Object.keys(filteredSettings).length === 0"
-      class="settings__empty"
+        class="settings"
+        :style="{
+            '--primary-color': appSettings.primary_color,
+            '--secondary-color': appSettings.secondary_color,
+        }"
     >
-      <i class="fa-solid fa-exclamation"></i>
-      <p>{{ $t("settings.no_results") }}</p>
+        <div class="settings__header">
+            <div class="settings__header__title">
+                <h3 class="settings__header__title__text">
+                    {{ $t("settings.label") }}
+                </h3>
+            </div>
+            <div class="settings__header__search">
+                <input
+                    v-model="search"
+                    class="settings__header__search__field"
+                    :placeholder="t('settings.search_placeholder')"
+                />
+                <i
+                    v-if="search"
+                    class="fa-solid fa-xmark settings__header__search__clear"
+                    @click="clearSearch"
+                ></i>
+            </div>
+        </div>
+        <div class="settings__list">
+            <div
+                v-for="(column, colIndex) in settingsColumns"
+                :key="colIndex"
+                class="settings__list__column"
+            >
+                <div
+                    v-for="[key, s] in column"
+                    :key="key"
+                    class="settings__list__section"
+                >
+                    <div class="settings__list__section__header">
+                        <div class="settings__list__section__header__title">
+                            <h6>{{ $t(s.label) }}</h6>
+                        </div>
+                        <div class="settings__list__section__header__desc">
+                            <p>{{ $t(s.description) }}</p>
+                        </div>
+                    </div>
+                    <div class="settings__list__section__links">
+                        <Link v-for="i in s.items" :href="i.path">
+                            <i :class="i.icon"></i>
+                            <span class="label">{{ $t(i.label) }}</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div
+            v-if="Object.keys(filteredSettings).length === 0"
+            class="settings__empty"
+        >
+            <i class="fa-solid fa-exclamation"></i>
+            <p>{{ $t("settings.no_results") }}</p>
+        </div>
     </div>
-  </div>
 </template>
